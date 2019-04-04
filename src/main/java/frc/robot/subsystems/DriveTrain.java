64d23e6c42336198b39e90efac935ffc33246b5c
Robert Paul <rob.k.paul@gmail.com>
2019-04-04 09:32:43 -0700

    Finished Rotation Drive Train - need to finish holo w/ cosine equations

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+import frc.robot.RobotMap;
+
 /**
  * 3 Wheel Holonomic Drive
  * 
@@ -22,9 +24,9 @@ public class DriveTrain extends Subsystem {
   private SpeedController motorC;
 
   public DriveTrain(){
-    motorA = new Talon(0);
-    motorB = new Talon(1);
-    motorC = new Talon(2);
+    motorA = new Talon(RobotMap.MOTOR_A_ID);
+    motorB = new Talon(RobotMap.MOTOR_B_ID);
+    motorC = new Talon(RobotMap.MOTOR_C_ID);
   }
 
 
@@ -37,17 +39,20 @@ public class DriveTrain extends Subsystem {
 
     
 
-    double theta = Math.toDegrees(Math.atan(y/x)));
+    double theta = Math.toDegrees(Math.atan(y/x));
     double r = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
     double speed = maxSpeed * r;
 
-    double aTheta = 90-theta;
-    double bTheta = 210-theta;
-    double cTheta = 330-theta;
+    double motorATheta = 90-theta;
+    double motorBTheta = 210-theta;
+    double motorCTheta = 330-theta;
 
-    double aSpeed = speed*Math.sin(aTheta) < 1.0 ? speed*Math.sin(aTheta) : 1.0;
-    double bSpeed = speed*Math.sin(bBheta) < 1.0 ? speed*Math.sin(bTheta) : 1.0;
-    double cSpeed = speed*Math.sin(cTheta) < 1.0 ? speed*Math.sin(cTheta) : 1.0;
+
+
+    double aSpeed = speed*Math.sin(motorATheta) < 1.0 ? speed*Math.sin(motorATheta) : 1.0;
+    double bSpeed = speed*Math.sin(motorBTheta) < 1.0 ? speed*Math.sin(motorBTheta) : 1.0;
+    double cSpeed = speed*Math.sin(motorCTheta) < 1.0 ? speed*Math.sin(motorCTheta) : 1.0;
+
 
     motorA.set(aSpeed);
     motorB.set(bSpeed);
@@ -57,6 +62,11 @@ public class DriveTrain extends Subsystem {
   }
 
   public void rotationDrive(double zValue) {
+    double z = zValue;
+
+    motorA.set(z);
+    motorB.set(z);    
+    motorC.set(z); 
 
   }
 
