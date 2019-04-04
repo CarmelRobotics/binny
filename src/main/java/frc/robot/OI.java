3e2538ce0a4fe814cb4fb21f13693031dd87ce8c
Robert Paul <rob.k.paul@gmail.com>
2019-04-04 09:33:03 -0700

    Set up Joystick Buttons for turn rotation on and off

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -6,7 +6,11 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
-  
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.*;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
@@ -14,9 +18,14 @@ package frc.robot;
 public class OI {
 
   private static Joystick jStick;
+  private static JoystickButton rotation;
   
   public static void initialize() {
+    jStick = RobotMap.JOYSTICK_A;
+    
+    rotation = RobotMap.ROTATION_BUTTON;
 
-      jStick = RobotMap.JOYSTICK;
+    rotation.whileHeld(new Rotate());
   }
+  
 }
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,6 +7,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -15,6 +18,19 @@ package frc.robot;
  */
 public class RobotMap {
 
+  /* IDs */
   public static final int JOYSTICK_A_ID = 0;
+  public static final int ROTATION_BUTTON_ID = 1;
+
+  public static final int MOTOR_A_ID = 0;
+  public static final int MOTOR_B_ID = 1;
+  public static final int MOTOR_C_ID = 2;
+
+  /* Joystick and Buttons */
   public static final Joystick JOYSTICK_A = new Joystick(JOYSTICK_A_ID);
+
+  public static final JoystickButton ROTATION_BUTTON = new JoystickButton(JOYSTICK_A, ROTATION_BUTTON_ID);
+
+  /*Various other Variables*/
+  public static boolean rotating = false;
 }
